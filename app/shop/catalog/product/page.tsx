e366b5d71f258b00a405c0c578ef44e9da26a8f3
Dotdan5919 <dighodaro95@gmail.com>
2025-07-20 21:49:24 +0100

    -

--- app/shop/catalog/product/page.tsx
+++ app/shop/catalog/product/page.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/sections/Footer';
 import Header from '@/components/sections/Header';
 import Header_2 from '@/components/sections/Header_2';
+import ProductBody from '@/components/sections/ProductBody';
 import ClientProviders from '@/contexts/ClientProviders';
 import useShop from '@/hooks/useShop';
 import { useSearchParams } from 'next/navigation'
@@ -13,7 +14,7 @@ export default function page() {
 
     const searchParams=useSearchParams();
 
-    const value=searchParams.get('id') || "";
+    const value=searchParams.get('id') ;
 
     
     
@@ -22,7 +23,7 @@ export default function page() {
     <ClientProviders>
    <div className="h-fit w-screen overflow-hidden flex flex-col   gap-16 lg:pt-10 lg:pb-20  lg:px-32 p-8 pb-20">
       <Navbar/>
-      
+      <ProductBody value={value}/>
 
       
       
--- components/sections/ProductBody.tsx
+++ components/sections/ProductBody.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Header_2 from './Header_2'
 
-export default function ProductBody() {
+export default function ProductBody({value}) {
+
   return (
     <div>
       <Header_2 title={value} subtitle='' previous='Catalog' preprevious='Shop' />
